6674ab2714f111c3b3299474a52801f5384d5274
Shreyash4780 <153109266+Shreyash4780@users.noreply.github.com>
2025-03-23 17:25:30 +0530

    Update Q21.java

--- Assignment No. 1/Q21.java
+++ Assignment No. 1/Q21.java
@@ -2,5 +2,19 @@
 // -->
 
 public class Q21 {
-    
+    public static void main(String[] args) {
+        
+        Integer intObj = 10; 
+        Double doubleObj = 5.5;
+        Character charObj = 'A';
+
+        System.out.println("Integer: " + intObj);
+        System.out.println("Double: " + doubleObj);
+        System.out.println("Character: " + charObj);
+    }
 }
+
+// Output =>
+// Integer: 10
+// Double: 5.5
+// Character: A
