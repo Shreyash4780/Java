7c7c8825eb3e86abc3b0508099cca74f95a3a6f7
Shreyash4780 <153109266+Shreyash4780@users.noreply.github.com>
2025-03-23 17:26:16 +0530

    Update Q17.java

--- Assignment No. 1/Q17.java
+++ Assignment No. 1/Q17.java
@@ -1,6 +1,21 @@
 // 17. Write a java program to stop inheritance. 
 // -->
 
-public class Q17 {
-    
+final class One {
+    void show() {
+        System.out.println("This is a final class and cannot be inherited.");
+    }
 }
+
+// The following will cause an error because One is final
+// class Two extends Base { } // ERROR: Cannot inherit from final class
+
+public class Q17 {
+    public static void main(String[] args) {
+        One obj = new One();
+        obj.show();
+    }
+}
+
+// Output =>
+// This is a final class and cannot be inherited.
